0579ba4d63880ac8de941d2ede0f17c2b0bd56f1
luis queral <luismqueral@gmail.com>
2014-07-04 18:19:28 -0400

    changed sig

--- uploadtotumblr.py
+++ uploadtotumblr.py
@@ -12,7 +12,7 @@ OAUTHFILE=path.join(path.expanduser('~'),'.tumblr.oauth') #Comment this out and 
 #ACCESS_TOKEN_STORAGE_FILE='/path/to/file.oauth'
 POEM_PATH='/Users/luis/Dropbox/Public/poems'
 USETIMEDELTA=True
-TIMEDELTA=timedelta(minutes=5) #For this to work, the timezone of your Tumblr-account needs to be set correctly, and your system time should be accurate
+TIMEDELTA=timedelta(minutes=15) #For this to work, the timezone of your Tumblr-account needs to be set correctly, and your system time should be accurate
 #TIMEDELTA=timedelta(hours=1,minutes=2,seconds=10) #You get the idea...
 ###########################################
 CONSUMER_KEY='gcXNyTsZCHfH1ROf519M3jwHfAbSWNMawSV7PW1FrKCm6KtIJj'
