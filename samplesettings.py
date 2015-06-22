64ffa6f09ebc283c5c38c965f09fa02d2c8e86cb
Elon Bing <elon.bing@gmail.com>
2015-06-22 05:41:17 +0200

    Fixed minor bug with missing imports in (sample)settings.py

--- samplesettings.py
+++ samplesettings.py
@@ -1,13 +1,11 @@
 #Sample settings file. Rename to "settings.py" to use.
+from os import path
+from datetime import timedelta
 
 POEM_LENGTH=5
 
 MINSUBLENGTH=3 #Minimum length in characters for a caption
 MAX_LINES_PER_VIDEO=2 #Setting this to a higher number gives higher performance, but more lines from the same video
-SAVE_TO_FILE=True  #Set to False to output to stdout
-POEM_BASENAME="Poem-" #The current date and time get added to POEM_BASENAME, so for example Poem-2014-06-30T12.51.txt
-POEM_EXTENSION=".txt"
-POEM_PATH="/path/to/poetry/dir/"
 NUMBER_OF_POEMS=50 #Use this to make large amounts of poems
 WORDRANGE=xrange(3,6) #Anywhere from 1 to 7 words.
 #WORDRANGE=[1,3,7,8] #Only lines with either 1,3,7, or 8 words are accepted
